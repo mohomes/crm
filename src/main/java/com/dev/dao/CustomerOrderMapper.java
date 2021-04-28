e0293e19fc4f1d953d7b2eeabff296df611140e2
ning1_wang@163.com <ning1_wang@163.com>
2021-04-29 05:54:36 +0800

    crm项目管理系统-角色授权 1、菜单树前台显示完成 2、角色授权完成 3、前台菜单资源显示 4、后台菜单资源切面控制显示 5、角色权限认证完成 6、添加资源失败 7、客户管理数据表格后端实现 8、客户管理添加操作后端代码实现 9、客户信息管理-订单查看-订单详情查看

---
+++ src/main/java/com/dev/dao/CustomerOrderMapper.java
@@ -0,0 +1,12 @@
+package com.dev.dao;
+
+import com.dev.base.BaseMapper;
+import com.dev.vo.CustomerOrder;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
+
+public interface CustomerOrderMapper extends BaseMapper<CustomerOrder,Integer> {
+
+    Map<String, Object> queryOrderDetailByOrderId(Integer orderId);
+}
---
+++ src/main/java/com/dev/dao/OrderDetailsMapper.java
@@ -0,0 +1,8 @@
+package com.dev.dao;
+
+import com.dev.base.BaseMapper;
+import com.dev.vo.OrderDetails;
+
+public interface OrderDetailsMapper extends BaseMapper<OrderDetails,Integer> {
+    
+}
